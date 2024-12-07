bd9b88e7484680bc8e8a53566037aba9481c180d
kataia1216 <kataiattila20011216@gmail.com>
2024-12-07 12:51:00 +0100

    Delete Code, unit test directory

--- Code, unit test/JavaDoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"FodraszatApp"},{"p":"<Unnamed>","l":"FoglalasKezelo"},{"p":"<Unnamed>","l":"FoglalasKezeloTest"}];updateSearchResults();
